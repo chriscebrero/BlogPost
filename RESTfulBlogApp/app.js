72808221c061ea1a481ecfd2ffcb256e0bf4fc9e
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-08 05:17:58 +0000

    got index

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -20,7 +20,17 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
+Blog.create({
+    title: "TestBlog",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Feels_good_man.jpg/200px-Feels_good_man.jpg",
+    body: "Hello this is a blogpost"
+})
+
 //RESTFUL Routes
+
+app.get("/blogs", function(req, res){
+    res.render("index");
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running!");
