58163ea2bcddb9b8b96171ca438cabeae06a9738
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-08 05:10:16 +0000

    comments

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -8,11 +8,19 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-// title
-// image
-// body
-// created
 
+
+//MONGOOSE CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL Routes
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running!");
