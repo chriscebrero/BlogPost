64dc457971c9a9b7302c20ef5e91aa6a920696ec
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-08 05:07:12 +0000

    setup done

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -5,8 +5,15 @@ mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 // title
 // image
 // body
 // created
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server is running!");
+});
